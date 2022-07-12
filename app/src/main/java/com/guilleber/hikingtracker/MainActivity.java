3f3a025d274b2c293678601fe6f3374b208481ca
guilleber <guillaume.le_berre@depinfonancy.net>
2022-07-12 15:43:19 -0400

    Very simple start

--- app/src/main/java/com/guilleber/hikingtracker/MainActivity.java
+++ app/src/main/java/com/guilleber/hikingtracker/MainActivity.java
@@ -3,6 +3,12 @@ package com.guilleber.hikingtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameEdit = findViewById(R.id.name_edittext);
+        mNameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mStartButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+
+        mStartButton = findViewById(R.id.start_button);
+        mStartButton.setEnabled(false);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNameEdit.setEnabled(false);
+                mEndButton.setEnabled(true);
+                mStartButton.setVisibility(View.GONE);
+                mPauseButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mPauseButton = findViewById(R.id.pause_button);
+
+        mEndButton = findViewById(R.id.end_button);
+        mEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mEndButton.setEnabled(false);
     }
+
+    private EditText mNameEdit;
+    private Button mStartButton;
+    private ToggleButton mPauseButton;
+    private Button mEndButton;
 }
