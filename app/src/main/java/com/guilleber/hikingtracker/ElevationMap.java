7229e3499daf1e29dc6446e4a1a03d1c388aecba
guilleber <guillaume.le_berre@depinfonancy.net>
2022-07-16 23:25:08 -0400

    bug fix on elevation map

--- app/src/main/java/com/guilleber/hikingtracker/ElevationMap.java
+++ app/src/main/java/com/guilleber/hikingtracker/ElevationMap.java
@@ -2,21 +2,24 @@ package com.guilleber.hikingtracker;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import java.util.Vector;
 
 public class ElevationMap extends View {
     private Paint mLinePaint;
+    private Paint mDashPaint;
+    private TextPaint mTextPaint;
 
     private int mWidth;
     private int mHeight;
     private int[] mMinMax = new int[4];
     private int mLastMinMaxUpdate = 0;
+    private final int mMarginSize = 50;
 
     private Vector<Integer> mAltMemory = new Vector<>();
 
@@ -38,6 +41,16 @@ public class ElevationMap extends View {
         mLinePaint.setStrokeWidth(5);
         mLinePaint.setColor(0xFF26263A);
 
+        mDashPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mDashPaint.setStyle(Paint.Style.STROKE);
+        mDashPaint.setStrokeWidth(3);
+        mDashPaint.setColor(0xFF26263A);
+        mDashPaint.setPathEffect(new DashPathEffect(new float[]{20, 10}, 0));
+
+        mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(0xFF26263A);
+        mTextPaint.setTextSize(30);
+
         setBackgroundColor(0x00FFFFFF);
     }
 
@@ -46,28 +59,34 @@ public class ElevationMap extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         int wPad = getPaddingLeft() + getPaddingRight();
         int hPad = getPaddingTop() + getPaddingBottom();
-        mWidth = w - wPad;
-        mHeight = h - hPad;
+        mWidth = w - wPad - 2*mMarginSize;
+        mHeight = h - hPad - 2*mMarginSize;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        computeMinMax();
-        if(mMinMax[3] <= mMinMax[1])
+        if(mAltMemory.size() < 2)
             return;
+
+        computeMinMax();
 
         float yRatio = (float)Math.max(mMinMax[3] - mMinMax[1], 50);
         yRatio = mHeight/yRatio;
 
-        int nbPoints = Math.min(mAltMemory.size(), (int)(mWidth/4.0));
-        float xRatio = mWidth/(float)nbPoints;
+        int nbPoints = Math.min(mAltMemory.size(), mWidth/4);
+        float xRatio = mWidth/(float)(nbPoints - 1);
 
         if(nbPoints < 2)
             return;
 
+        canvas.drawText(mMinMax[3] + " m", mMarginSize, mHeight + mMarginSize - yRatio*(mMinMax[3] - mMinMax[1]) - 10, mTextPaint);
+        canvas.drawLine(mMarginSize, mHeight + mMarginSize - yRatio*(mMinMax[3] - mMinMax[1]), mWidth + mMarginSize, mHeight + mMarginSize - yRatio*(mMinMax[3] - mMinMax[1]), mDashPaint);
+        canvas.drawText(mMinMax[1] + " m", mMarginSize, mHeight + mMarginSize + 30, mTextPaint);
+        canvas.drawLine(mMarginSize, mHeight + mMarginSize, mWidth + mMarginSize, mHeight + mMarginSize, mDashPaint);
+
         for(int i = mAltMemory.size() - nbPoints + 1; i < mAltMemory.size(); i++) {
-            canvas.drawLine(xRatio*(i-1), mHeight - yRatio*mAltMemory.elementAt(i-1), xRatio*i, mHeight - yRatio*mAltMemory.elementAt(i), mLinePaint);
+            canvas.drawLine(mMarginSize + xRatio*(i-1), mHeight + mMarginSize - yRatio*(mAltMemory.elementAt(i-1) - mMinMax[1]), mMarginSize + xRatio*i, mHeight + mMarginSize - yRatio*(mAltMemory.elementAt(i) - mMinMax[1]), mLinePaint);
         }
     }
 
@@ -76,14 +95,14 @@ public class ElevationMap extends View {
     }
 
     private void computeMinMax() {
-        for(int i = mLastMinMaxUpdate; i < mAltMemory.size(); i++) {
-            if(mAltMemory.elementAt(i) >= mMinMax[3]) {
-                mMinMax[2] = i;
-                mMinMax[3] = mAltMemory.elementAt(i);
+        for(; mLastMinMaxUpdate < mAltMemory.size(); mLastMinMaxUpdate++) {
+            if(mAltMemory.elementAt(mLastMinMaxUpdate) >= mMinMax[3]) {
+                mMinMax[2] = mLastMinMaxUpdate;
+                mMinMax[3] = mAltMemory.elementAt(mLastMinMaxUpdate);
             }
-            if(mAltMemory.elementAt(i) <= mMinMax[1]) {
-                mMinMax[0] = i;
-                mMinMax[1] = mAltMemory.elementAt(i);
+            if(mAltMemory.elementAt(mLastMinMaxUpdate) >= 0 && mAltMemory.elementAt(mLastMinMaxUpdate) <= mMinMax[1]) {
+                mMinMax[0] = mLastMinMaxUpdate;
+                mMinMax[1] = mAltMemory.elementAt(mLastMinMaxUpdate);
             }
         }
     }
